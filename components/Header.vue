96cac353c33a63fe08b5994ac6218d09eccbb9a0
Denis <polaykov_d@ukr.net>
2020-11-08 18:05:51 +0200

    new commit

---
+++ components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app-bar class="main__navigation" color="black" fixed app elevate-on-scroll>
+    <v-container fluid class="pa-0">
+      <div class="d-flex justify-space-between">
+        <v-img
+          src="/logo.png"
+          max-height="40"
+          max-width="80"></v-img>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn v-for="(item, index) in items" :key="index" text>
+            {{ item.title }}
+          </v-btn>
+        </v-toolbar-items>
+        <v-menu offset-y transition="fab-transition">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              dark
+              class="hidden-md-and-up"
+              v-bind="attrs"
+              v-on="on"
+            >
+              Меню
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(item, index) in items"
+              :key="index"
+            >
+              <v-list-item-title class="menu__link">
+                {{ item.title }}
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      items: [
+        { title: 'Главная' },
+        { title: 'Услуги' },
+        { title: 'Про нас' },
+        { title: 'Кейсы' },
+        { title: 'Контакты' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    max-width: 1300px !important;
+  }
+  .menu__link {
+    cursor: pointer;
+  }
+  .menu__link:hover {
+    border-bottom: 1px solid #fff;
+    margin-bottom: -1px;
+  }
+</style>
