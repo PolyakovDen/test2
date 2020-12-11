843594e20e3ac579ca83bc45cc21decbb0b1a7ee
Denis <polaykov_d@ukr.net>
2020-12-11 18:19:04 +0200

    add new style for success page

--- pages/success.vue
+++ pages/success.vue
@@ -1,17 +1,15 @@
 <template>
-<!--  <div class="success&#45;&#45;page">-->
-    <v-container fluid fill-height class="success--page">
-      <v-row justify="center" align="center">
-        <v-col lg="7" md="8" sm="10">
-          <div class="success__messages pt-10 pb-10">
-            <p class="text-center success__messages--text">Спасибо за ваше сообщение.</p>
-            <p class="text-center success__messages--text">Мы свяжемся с вами в ближайшее время.</p>
-            <nuxt-link class="link__home pl-10" to="/"><- Вернуться на главную</nuxt-link>
-          </div>
-        </v-col>
-      </v-row>
-    </v-container>
-<!--  </div>-->
+  <v-container fluid fill-height class="success--page">
+    <v-row justify="center" align="center">
+      <v-col lg="7" md="8" sm="10">
+        <div class="success__messages pt-10 pb-10">
+          <p class="text-center success__messages--text">Спасибо за ваше сообщение.</p>
+          <p class="text-center success__messages--text">Мы свяжемся с вами в ближайшее время.</p>
+          <nuxt-link class="link__home pl-10" to="/">< Вернуться на главную</nuxt-link>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -22,18 +20,16 @@
 
 <style scoped>
   .success--page {
-    background-color: #0F4C5C;
+    background-color: #2f6565;
   }
   .success__messages {
     border-radius: 10px;
-    background-color: #E36414;
+    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
   }
   .success__messages--text {
     font-size: 21px;
-    color: #5F0F40;
   }
   .link__home {
     text-decoration: none;
-    color: #0f4c5c;
   }
 </style>
